7fc34d6e7eea97fa71b046d9ad903c7efdac7004
jrkozak92 <jrkozak92@gmail.com>
2022-01-26 21:50:39 -0600

    navbar functionality and project population

--- app/app.js
+++ app/app.js
@@ -4,10 +4,32 @@ const projects = [{name: 'test',
 
 $(() => {
   for (let obj of projects) {
-    let project = $('<div>')
+    let $project = $('<div>')
                   .addClass('project')
                   .attr('id',`project-${projects.indexOf(obj)}`)
-    $('body').append(project)
+
+    let $image = $('<img>')
+                .attr('src', obj.imageRef)
+                .addClass('project-thumbnail')
+
+    let $title = $('<h6>')
+                .text(obj.name)
+                .addClass('project-title')
+
+    let $description = $('<p>')
+                      .text(obj.description)
+                      .addClass('project-description')
+
+    $project.append($image)
+    $project.append($title)
+    $project.append($description)
+    $('#project-container').append($project)
+    $('#closed-nav').on('click', () => {
+      $('#open-nav').toggleClass('hide')
+    })
+    $('ion-icon').on('click', () => {
+      $('#open-nav').toggleClass('hide')
+    })
   }
 
 })
